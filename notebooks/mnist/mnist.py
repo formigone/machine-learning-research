c7b62a261ecdc9affd3fc63eceb0cc7b89ab2ccc
Rodrigo Silveira <rsilveira@deseretdigital.com>
2017-09-13 23:55:46 -0600

    Training for a few hours

--- notebooks/mnist/mnist.py
+++ notebooks/mnist/mnist.py
@@ -129,7 +129,7 @@ def main(_):
     cross_entropy = tf.reduce_mean(cross_entropy)
 
     with tf.name_scope('adam_optimizer'):
-        train_step = tf.train.AdamOptimizer(1e-4).minimize(cross_entropy)
+        train_step = tf.train.AdamOptimizer(1e-6).minimize(cross_entropy)
 
     with tf.name_scope('accuracy'):
         correct_prediction = tf.equal(tf.argmax(y_conv, 1), tf.argmax(y_, 1))
@@ -150,13 +150,14 @@ def main(_):
             train_writer.add_graph(tf.get_default_graph())
 
             mnist = input_data.read_data_sets(FLAGS.data_dir, one_hot=True)
-            for i in range(20000):
+            for i in range(100000):
                 batch = mnist.train.next_batch(FLAGS.training_batch)
                 train_step.run(feed_dict={x: batch[0], y_: batch[1], keep_prob: 0.5})
 
                 if i % 100 == 0:
                     train_accuracy = accuracy.eval(feed_dict={x: batch[0], y_: batch[1], keep_prob: 1.0})
-                    print('step %d, training accuracy %g' % (i, train_accuracy))
+                    loss = cross_entropy.eval(feed_dict={x: batch[0], y_: batch[1], keep_prob: 1.0})
+                    print('step %d, training accuracy %g  loss %g' % (i, train_accuracy, loss))
                 if i % 500 == 0:
                     print('Accuracy %g' % accuracy.eval(feed_dict={x: mnist.test.images, y_: mnist.test.labels, keep_prob: 1.0}))
                     save_path = saver.save(sess, FLAGS.session_dir)
